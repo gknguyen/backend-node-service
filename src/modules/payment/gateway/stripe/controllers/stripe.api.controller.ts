a96aadda84bd3da54d555cbab53c1bbed2a5379d
gknguyen <nguyentruonggiang1711@gmail.com>
2024-10-09 21:39:01 +0700

    chore: refactor StripeApiController

--- src/modules/payment/gateway/stripe/controllers/stripe.api.controller.ts
+++ src/modules/payment/gateway/stripe/controllers/stripe.api.controller.ts
@@ -4,51 +4,51 @@ import { StripeService } from '../services/stripe.service';
 import { StripeAddCustomerCardInputDto, StripeChargeInputDto } from '../shared/stripe.dto';
 
 @ApiTags('Payment Gateway - Stripe')
-@Controller({ path: 'payment-gateway/user/stripe' })
+@Controller({ path: 'payment-gateway/stripe' })
 export class StripeApiController {
   constructor(private readonly stripeService: StripeService) {}
 
   @Get('customers/:email')
   @ApiOperation({
-    operationId: 'userGetStripeCustomer',
+    operationId: 'getStripeCustomer',
     summary: 'Get Stripe customer information by User.',
   })
-  userGetStripeCustomer(@Param('email') email: string) {
+  getStripeCustomer(@Param('email') email: string) {
     return this.stripeService.getCustomer(email);
   }
 
   @Post('customers/cards')
   @ApiOperation({
-    operationId: 'userAddStripeCustomerCards',
+    operationId: 'addStripeCustomerCards',
     summary: 'Add card for Stripe Customer by User.',
   })
   @ApiBody({
     description: StripeAddCustomerCardInputDto.name,
     type: StripeAddCustomerCardInputDto,
   })
-  userAddStripeCustomerCards(@Body() body: StripeAddCustomerCardInputDto) {
+  addStripeCustomerCards(@Body() body: StripeAddCustomerCardInputDto) {
     return this.stripeService.addCustomerCard(body.email, body);
   }
 
   @Delete('customers/:email/cards/:id')
   @ApiOperation({
-    operationId: 'userRemoveStripeCustomerCard',
+    operationId: 'removeStripeCustomerCard',
     summary: 'Remove card for Stripe Customer by User.',
   })
-  userRemoveStripeCustomerCard(@Param('email') email: string, @Param('id') cardId: string) {
+  removeStripeCustomerCard(@Param('email') email: string, @Param('id') cardId: string) {
     return this.stripeService.removeCustomerCard(email, cardId);
   }
 
   @Post('customers/:email/charge')
   @ApiOperation({
-    operationId: 'userChargeStripeCustomer',
+    operationId: 'chargeStripeCustomer',
     summary: 'Charge transaction with Stripe gateway bu User.',
   })
   @ApiBody({
     description: StripeChargeInputDto.name,
     type: StripeChargeInputDto,
   })
-  userChargeStripeCustomer(@Param('email') email: string, @Body() body: StripeChargeInputDto) {
+  chargeStripeCustomer(@Param('email') email: string, @Body() body: StripeChargeInputDto) {
     return this.stripeService.charge(email, body);
   }
 }
