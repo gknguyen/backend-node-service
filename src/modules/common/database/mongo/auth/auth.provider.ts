35be142f516e85265bdfa774ba16655a0d517837
gknguyen <nguyentruonggiang1711@gmail.com>
2025-03-16 17:07:21 +0700

    feat: add migration script for mongoose

--- src/modules/common/database/mongo/auth/auth.provider.ts
+++ src/modules/common/database/mongo/auth/auth.provider.ts
@@ -1,22 +1,21 @@
 import { connect } from 'mongoose';
-import ENV from 'src/shared/env';
+import ENV from '../../../../../shared/env';
 import { MONGO_AUTH_TOKEN } from '../../shared/database.const';
 import { getConnectionString } from '../shared/mongo.utils';
+
+export const authConnectionString = getConnectionString({
+  host: ENV.MONGODB.AUTH.HOST,
+  port: ENV.MONGODB.AUTH.PORT,
+  username: ENV.MONGODB.AUTH.USERNAME,
+  password: ENV.MONGODB.AUTH.PASSWORD,
+  database: ENV.MONGODB.AUTH.DATABASE,
+});
 
 export const AuthDatabaseProvider = {
   provide: MONGO_AUTH_TOKEN,
   useFactory: () =>
-    connect(
-      getConnectionString({
-        host: ENV.MONGODB.AUTH.HOST,
-        port: ENV.MONGODB.AUTH.PORT,
-        username: ENV.MONGODB.AUTH.USERNAME,
-        password: ENV.MONGODB.AUTH.PASSWORD,
-        database: ENV.MONGODB.AUTH.DATABASE,
-      }),
-      {
-        autoIndex: false,
-        directConnection: ENV.MONGODB.AUTH.DIRECT_CONNECTION,
-      },
-    ),
+    connect(authConnectionString, {
+      autoIndex: false,
+      directConnection: ENV.MONGODB.AUTH.DIRECT_CONNECTION,
+    }),
 };
